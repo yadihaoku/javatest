844adcfa36476ba5cb3df7a31098c3562996706b
yadihaoku <yadihaoku@gmail.com>
2014-06-18 16:50:09 +0800

    first commit

---
+++ JavaTest/src/com/centfor/test/Main.java
@@ -0,0 +1,11 @@
+package com.centfor.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		boolean result = true;
+		boolean error = false;
+		System.out.println(result^=error);
+	}
+
+}
