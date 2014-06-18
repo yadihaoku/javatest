adb0b39cba9ec16edb69e3e72a3b64dfcd976294
yadihaoku <yadihaoku@gmail.com>
2014-06-18 16:52:01 +0800

    commit simple chinaese

--- JavaTest/src/com/centfor/test/Main.java
+++ JavaTest/src/com/centfor/test/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		boolean result = true;
 		boolean error = false;
+		System.out.println("这是什么 ");
 		System.out.println(result^=error);
 	}
 
